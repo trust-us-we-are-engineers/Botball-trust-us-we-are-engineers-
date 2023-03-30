c4464e85239860b9df773f39ad3851326fba6e93
trust-us-we-are-engineers <123667209+trust-us-we-are-engineers@users.noreply.github.com>
2023-03-30 09:19:10 +0200

    Update drive_until.c

--- usable_functions/drive_until.c
+++ usable_functions/drive_until.c
@@ -1,22 +1,22 @@
 void drive_until_line(){
     int speed=300;
     int sleep_time=25;
-    while(!(get_create_lcliff_amt()<on_line_go_to && get_create_rcliff_amt()<on_line_go_to)){
+    while(!(get_create_lcliff_amt()<on_line_go_to && get_create_rcliff_amt()<on_line_go_to)){// while both sensors are not on a black line
         if(get_create_lcliff_amt()<on_line_go_to){//turn clockwise and drive backwards
-                                                  //when left middle sensor is on the line
+                                                  //when left outer sensor is on the line
             create_spin_CCW(speed);
             msleep(sleep_time/3);
             create_drive_straight(-speed);
             msleep(sleep_time/6);
         }
         if(get_create_rcliff_amt()<on_line_go_to){//turn against the clock and drive backwards
-                                                  //when right middle sensor is on the line
+                                                  //when right outer sensor is on the line
             create_spin_CW(speed);
             msleep(sleep_time/3);
             create_drive_straight(-speed);
             msleep(sleep_time/6);
         }
-        else{// when no sensor is seeing black drive forward
+        else{// when no sensor is seeing no black drive forward
             create_drive_straight(speed);
             msleep(sleep_time/4);
         }
